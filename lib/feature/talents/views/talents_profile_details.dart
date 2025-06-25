66262a405b3e8da9ee8a11e3d6a5e9d74a2f8bc4
mahesh <>
2025-06-25 15:04:43 +0530

    talants list load issue fixed

---
+++ lib/feature/talents/views/talents_profile_details.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TalentsProfileDetails extends StatefulWidget {
+  const TalentsProfileDetails({ Key? key }) : super(key: key);
+
+  @override
+  _TalentsProfileDetailsState createState() => _TalentsProfileDetailsState();
+}
+
+class _TalentsProfileDetailsState extends State<TalentsProfileDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
