f94cfbe97d1525d54369c6d2f698090fe1484c6b
mahesh mobile <maheshy1908@gmail.com>
2025-07-19 10:53:31 +0530

    account changes

--- lib/feature/account/account_view_model/account_view_model.dart
+++ lib/feature/account/account_view_model/account_view_model.dart
@@ -1,6 +1,10 @@
+import 'package:di360_flutter/common/constants/local_storage_const.dart';
+import 'package:di360_flutter/data/local_storage.dart';
 import 'package:di360_flutter/feature/account/account_model/account_model.dart';
 import 'package:di360_flutter/feature/account/repository/account_repository.dart';
+import 'package:di360_flutter/utils/user_role_enum.dart';
 import 'package:flutter/foundation.dart';
+
 
 
 class ProfileViewModel extends ChangeNotifier {
@@ -8,26 +12,43 @@ class ProfileViewModel extends ChangeNotifier {
 
   ProfileViewModel(this._repository);
 
-  List<ProfileSection> _sections = [];
-  bool _isLoading = false;
+  ProfileSection? _profileSection;
+  List<ProfileCategory> _visibleSections = [];
+
   String? _error;
-
-  List<ProfileSection> get sections => _sections;
-  bool get isLoading => _isLoading;
+  ProfileSection? get profileSection => _profileSection;
+  List<ProfileCategory> get visibleSections => _visibleSections;
   String? get error => _error;
-
   Future<void> fetchProfileSections() async {
-    _isLoading = true;
     _error = null;
-    notifyListeners();
-
     try {
-      _sections = await _repository.getProfileSections();
+      _profileSection = await _repository.getProfileSections();
+      await _filterSectionsByRole();
     } catch (e) {
       _error = e.toString();
     }
-
-    _isLoading = false;
     notifyListeners();
+  }
+
+  Future<void> _filterSectionsByRole() async {
+    final type = await LocalStorage.getStringVal(LocalStorageConst.type);
+    final userRole = UserRole.fromString(type);
+
+    switch (userRole) {
+      case UserRole.professional:
+        _visibleSections = _profileSection?.roles["PROFESSIONAL"]?.data ?? [];
+        break;
+      case UserRole.practice:
+        _visibleSections = _profileSection?.roles["PRACTICE"]?.data ?? [];
+        break;
+      case UserRole.supplier:
+        _visibleSections = _profileSection?.roles["SUPPLIER"]?.data ?? [];
+        break;
+      case UserRole.admin:
+        _visibleSections = _profileSection?.roles["ADMIN"]?.data ?? [];
+        break;
+      default:
+        _visibleSections = [];
+    }
   }
 }
