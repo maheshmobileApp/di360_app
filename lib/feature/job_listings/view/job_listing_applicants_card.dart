acd0df9ed33c9a360a4cdad5f1f8d1623990ead6
mahesh mobile <maheshy1908@gmail.com>
2025-08-31 17:13:45 +0530

    jobapplicantsapicanhegs

--- lib/feature/job_listings/view/job_listing_applicants_card.dart
+++ lib/feature/job_listings/view/job_listing_applicants_card.dart
@@ -123,10 +123,27 @@ class JobListingApplicantsCard extends StatelessWidget with BaseContextHelpers {
                 children: [
                   _roundedButton("Resume"),
                   addHorizontal(10),
-                  InkWell(onTap: () {
-                     navigationService
-                                .navigateTo(RouteList.JobListingApplicantsMessege); 
-                  }, child: _roundedButton("Message")),
+               InkWell(
+  onTap: () {
+    if (applicant == null || applicant.id == null || applicant.jobId == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Applicant or Job ID not available")),
+      );
+      return;
+    }
+
+  navigationService.navigateToWithParams(
+  RouteList.JobListingApplicantsMessege,
+  params: {
+    "jobId": applicant?.jobId ?? "",
+    "applicantId": applicant?.id ?? "",
+  },
+);
+
+  },
+  child: _roundedButton("Message"),
+),
+
                   addHorizontal(10),
                   InkWell(
                       onTap: () {
