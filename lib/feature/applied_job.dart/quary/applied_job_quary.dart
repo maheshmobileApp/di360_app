81bf8bf5db00be43704830ddb57bb6996be521ef
mahesh <>
2025-09-02 22:23:10 +0530

    changed the quey

--- lib/feature/applied_job.dart/quary/applied_job_quary.dart
+++ lib/feature/applied_job.dart/quary/applied_job_quary.dart
@@ -8,17 +8,50 @@ query getajobswithallprofiles($dental_professional_id:uuid) {
   message
    dental_professional_id
     job{
-      country
-      company_name
-      experience
-      salary
-      status
-      banner_image
-      TypeofEmployment
-      active_status
-      created_at
-      clinic_logo
-      description
+      id
+    title
+    j_type
+    j_role
+    description
+    TypeofEmployment
+    years_of_experience
+    dental_practice_id
+    dental_supplier_id
+    location
+    logo
+    state
+    city
+    company_name
+    salary
+    website_url
+    pay_range
+    education
+    video
+    rate_billing
+    created_at
+    job_applicants_aggregate {
+      aggregate {
+        count
+      }
+    }
+    country
+    days_of_week
+    current_company
+    endDateToggle
+    experience
+    facebook_url
+    hiring_period
+    instagram_url
+    is_featured
+    closing_message
+    closed_at
+    clinic_logo
+    banner_image
+    availability_date
+    address
+    active_status,
+    pay_min,
+    pay_max,
       job_enquiries {
         id
         enquiry_userid
@@ -28,5 +61,4 @@ query getajobswithallprofiles($dental_professional_id:uuid) {
     }
   }
  }
-
 ''';
