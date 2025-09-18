fc66fcd25935b50760225f1242ac4757b839a546
mahesh mobile <maheshy1908@gmail.com>
2025-09-18 21:16:07 +0530

    PReview ui

--- lib/feature/job_profile_listing/repository/job_profle_repository.dart
+++ lib/feature/job_profile_listing/repository/job_profle_repository.dart
@@ -1,4 +1,6 @@
-import 'package:di360_flutter/feature/job_profile/model/job_profile.dart';
+//import 'package:di360_flutter/feature/job_profile/model/job_profile.dart';
+import 'package:di360_flutter/feature/talents/model/job_profile.dart';
+
 abstract class JobProfileRepository {
   Future<List<JobProfile>> getJobProfiles();
    Future<void>updateJobProfile(String? id, String status);
