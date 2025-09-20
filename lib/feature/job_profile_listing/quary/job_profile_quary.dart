4f74542769e6e75d338a440640850d10d1f55486
mahesh <>
2025-09-21 00:39:46 +0530

    changed the jobprofile query

--- lib/feature/job_profile_listing/quary/job_profile_quary.dart
+++ lib/feature/job_profile_listing/quary/job_profile_quary.dart
@@ -1,26 +1,52 @@
-const String  jobProfileListing = r''' 
+const String jobProfileListing = r'''
 query GetJobProfileData($dental_professional_id: uuid!) {
   job_profiles(
     where: {dental_professional_id: {_eq: $dental_professional_id}}
     order_by: {created_at: desc}
   ) {
-    id
     created_at
     updated_at
+    skills
+    jobexperiences
+    educations
+    upload_resume
     job_designation
     current_company
+    current_ctc
     dental_professional_id
     post_anonymously
     admin_status
-    active_status
     profile_image
     full_name
+    mobile_number
+    email_address
     work_type
     profession_type
+    location
+    country
+    city
+    state
+    cover_letter
+    certificate
+    radius
+    abn_number
     availabilityOption
     availabilityDate
     fromDate
     availabilityDay
+    work_rights
+    Year_of_experiance
+    languages_spoken
+    areas_expertise
+    skills
+    percentage
+    salary_amount
+    salary_type
+    aphra_number
+    willing_to_travel
+    travel_distance
+    about_yourself
+    location
     availabilityType
     unavailabilityDate
     dental_professional {
