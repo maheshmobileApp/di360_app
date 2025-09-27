f30dd076590019393e47d01e8e462314510f6206
mahesh mobile <maheshy1908@gmail.com>
2025-09-27 11:21:02 +0530

    apporveBannerapi

--- lib/feature/banners/repository/banner_repository_impl.dart
+++ lib/feature/banners/repository/banner_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:di360_flutter/core/http_service.dart';
+import 'package:di360_flutter/feature/banners/model/approve_banner_model.dart';
 import 'package:di360_flutter/feature/banners/model/edit_banner_view_model.dart';
 import 'package:di360_flutter/feature/banners/model/get_banners.dart';
 import 'package:di360_flutter/feature/banners/model/get_category_list.dart';
@@ -6,6 +7,7 @@ import 'package:di360_flutter/feature/banners/quary/add_banner_quary.dart';
 import 'package:di360_flutter/feature/banners/quary/category_quary.dart';
 import 'package:di360_flutter/feature/banners/quary/delete_banner_query.dart';
 import 'package:di360_flutter/feature/banners/quary/edit_banner_view.dart';
+import 'package:di360_flutter/feature/banners/quary/get_approve_banner.dart';
 import 'package:di360_flutter/feature/banners/quary/get_banners_query.dart';
 import 'package:di360_flutter/feature/banners/quary/update_banner_query.dart';
 import 'package:di360_flutter/feature/banners/repository/banners_repository.dart';
@@ -49,10 +51,25 @@ class BannerRepositoryImpl extends BannersRepository {
   }
   
   @override
-  
   Future<dynamic> updateBanner(variables) async {
     final data = await http.mutation(updateBannerQuary, variables);
     return data;
   }
- 
+  
+ @override
+  Future<ApproveBannerResp> getApprovedBanners(dynamic variables) async {
+    final response = await http.query(
+      approveBannerQuary,
+      variables: variables ?? {
+        "limit": 10,
+        "offset": 0,
+      },
+    );
+
+    if (response != null && response['data'] != null) {
+      return ApproveBannerResp.fromJson(response);
+    } else {
+      throw Exception("Empty response received from server");
+    }
+  }
 }
