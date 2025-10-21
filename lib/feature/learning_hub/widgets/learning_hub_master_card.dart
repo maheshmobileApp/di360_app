e8cda953215f3a7a01fa7dda3e6d37d64105cfa0
Naniteppala118 <120006022+Naniteppala118@users.noreply.github.com>
2025-10-21 20:35:56 +0530

    jobseek view issue fixed

--- lib/feature/learning_hub/widgets/learning_hub_master_card.dart
+++ lib/feature/learning_hub/widgets/learning_hub_master_card.dart
@@ -1,4 +1,5 @@
 import 'package:di360_flutter/common/constants/app_colors.dart';
+import 'package:di360_flutter/common/constants/txt_styles.dart';
 import 'package:di360_flutter/feature/learning_hub/widgets/register_button.dart';
 import 'package:di360_flutter/widgets/cached_network_image_widget.dart';
 import 'package:flutter/material.dart';
