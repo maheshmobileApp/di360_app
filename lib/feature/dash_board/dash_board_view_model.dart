515396ba80f8f1cd8d61bf8f219bfeb7de6e84f7
Naniteppala118 <120006022+Naniteppala118@users.noreply.github.com>
2026-03-23 21:33:38 +0530

    dashboard issue fixed

--- lib/feature/dash_board/dash_board_view_model.dart
+++ lib/feature/dash_board/dash_board_view_model.dart
@@ -32,7 +32,7 @@ import 'package:url_launcher/url_launcher.dart';
 
 class DashBoardViewModel extends ChangeNotifier {
   int _currentIndex = 0;
-  int? _pendingIndex;
+  //int? _pendingIndex;
   List<Widget> _pages = [];
   String _userType = '';
   bool _isInitialized = false;
@@ -40,7 +40,7 @@ class DashBoardViewModel extends ChangeNotifier {
   int get currentIndex => _currentIndex;
   List<Widget> get pages => _pages;
   bool get isInitialized => _isInitialized;
-  String get userType => _userType;
+  //String get userType => _userType;
 
   DashBoardViewModel() {
     _initializePages();
@@ -52,10 +52,10 @@ class DashBoardViewModel extends ChangeNotifier {
     await LocalStorage.setBoolValue(
         LocalStorageConst.firstNavigationDirectory, false);
     _setupPages();
-    if (_pendingIndex != null && _pendingIndex! < _pages.length) {
+    /*if (_pendingIndex != null && _pendingIndex! < _pages.length) {
       _currentIndex = _pendingIndex!;
       _pendingIndex = null;
-    }
+    }*/
     _isInitialized = true;
     notifyListeners();
   }
@@ -96,16 +96,16 @@ class DashBoardViewModel extends ChangeNotifier {
   }
 
   void setIndex(int index, BuildContext context) {
-    if (_pages.isEmpty) {
+   /* if (_pages.isEmpty) {
       _pendingIndex = index;
       return;
     }
-    if (index < 0 || index >= _pages.length) return;
+    if (index < 0 || index >= _pages.length) return;*/
     _currentIndex = index;
     updateIndex(index, context);
     notifyListeners();
   }
-
+  
   updateIndex(int index, BuildContext context) async {
     if (_userType == UserRole.supplier.value) {
       switch (index) {
